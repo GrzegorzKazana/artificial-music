e23f11dde607c75c59de26c6b2e103db35b5f6f0
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-09-29 09:45:17 +0200

    wrote simple generating strategies

---
+++ src/generating/generating.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def recurrent_generate(model, seed, seq_length, window_size, is_binary=False):
+    """
+    Predicts only next step each iteration,
+    and then appends generated step to input,
+    iterations lasts until desired length is achived.
+    Assumes model is returning sequences of dim=3
+    """
+    x = seed
+    accum = [seed]
+    for i in range(seq_length - window_size):
+        res = model.predict(x)
+        next_timestep = res[:, -1, :].round() if is_binary else res[:, -1, :]
+        x = np.concatenate([x, next_timestep], axis=1)[:, -window_size, :]
+        accum.append(next_timestep)
+
+    return np.concatenate(accum, axis=1)
+
+
+def linear_generate(model, seed, is_binary=False):
+    """
+    Transforms given sequence in latent space.
+    Assumes model is returning sequences of dim=3
+    """
+    res = model.predict(seed)
+    return res.round() if is_binary else res
--- taskboard.md
+++ taskboard.md
@@ -5,10 +5,12 @@
 
 # IN PROGRESS
 
+- Write code decoding generated embedded samples (embedded -> npz -> mid)
 - Write first model for benchmarking
 - Write data loading scripts
 
 # COMPLETED
 
+- Write generation strategies
 - Train embedding
 - Import/reuse old code
