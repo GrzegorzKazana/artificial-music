ed0b58915c1be56e499a4109ba58c36defcc594f
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-10-02 20:33:23 +0200

    updated callback to log epochs passed

--- src/training/common/training_callbacks.py
+++ src/training/common/training_callbacks.py
@@ -21,16 +21,19 @@ class ModelAndLogSavingCallback(K.callbacks.Callback):
         self.model = model
 
     def on_epoch_end(self, epoch, logs={}):
+        self.logs.append(logs)
+
         if self.output_path == '':
             return
 
         t = datetime.now().isoformat().split('.')[0]
-        K.models.save_model(self.model, self.output_path + f'md{t}.h5')
+        epoch_count = len(self.logs)
+        K.models.save_model(self.model, self.output_path +
+                            f'md_e{epoch_count}_t{t}.h5')
 
         with open(os.path.join(self.output_path, 'log.json'), 'w+') as fo:
             json.dump(self.logs, fo, default=default, indent=4)
 
-        self.logs.append(logs)
         return
 
 
@@ -46,8 +49,6 @@ class GeneratingAndPlottingCallback(K.callbacks.Callback):
         t = datetime.now().isoformat().split('.')[0]
         seed = self.seed_generator()
         sample = self.sample_generator(self.model, seed)
-        print(sample)
-        print(np.array(sample).shape)
 
         _, axs = plt.subplots(nrows=4, ncols=4, figsize=(30, 10),
                               subplot_kw={'xticks': [], 'yticks': []})
