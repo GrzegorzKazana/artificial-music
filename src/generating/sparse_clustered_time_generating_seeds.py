815977a6fa5f48ba24fcdaf3a6692b4abc84680e
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-11-05 19:52:47 +0100

    prepared notebook withoout embedding

---
+++ src/generating/sparse_clustered_time_generating_seeds.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from src.generating.sparse_generating_seeds import seed_generators as sparse_seed_generators
+
+DEFAULT_DURATION_CLUSTER_SIZE = 24
+
+
+def get_random_duration(length, batch_size):
+    res = np.zeros((batch_size * length, DEFAULT_DURATION_CLUSTER_SIZE))
+
+    for i in range(batch_size * length):
+        res[i, np.random.randint(DEFAULT_DURATION_CLUSTER_SIZE)] = 1
+
+    return res.reshape((batch_size, length, DEFAULT_DURATION_CLUSTER_SIZE))
+
+
+def wrap_seed_generator_w_duration(seed_gen):
+    def inner(length, input_size, **kwargs):
+        batch_size = kwargs['batch_size'] or 16
+        seed = seed_gen(length, input_size, **kwargs)
+        duration = get_random_duration(length, batch_size)
+
+        return np.concatenate((seed, duration), axis=2)
+
+    return inner
+
+
+seed_generators = {
+    k: wrap_seed_generator_w_duration(v) for k, v in sparse_seed_generators.items()
+}
