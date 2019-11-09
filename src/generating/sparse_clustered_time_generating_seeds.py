5cdb8d87e8d8a73a3898917c3de50c7ebf88ac7e
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-11-09 16:05:02 +0100

    adjusted wrapping with clusterd time ohe
    now durations are related to occurence propability

--- src/generating/sparse_clustered_time_generating_seeds.py
+++ src/generating/sparse_clustered_time_generating_seeds.py
@@ -5,23 +5,38 @@ from src.generating.sparse_generating_seeds import random_noise_seed, zero_seed,
 DEFAULT_DURATION_CLUSTER_SIZE = 24
 
 
-def get_random_duration(length, batch_size):
+def get_random_duration(length, batch_size, duration_dict, ignore_shortest):
     res = np.zeros((batch_size * length, DEFAULT_DURATION_CLUSTER_SIZE))
 
+    durations = [int(d) for d in duration_dict.keys()]
+    dur_count = [v['count'] for v in duration_dict.values()]
+    dur_avg = [v['avg'] for v in duration_dict.values()]
+
+    # filter shortest
+    if ignore_shortest:
+        durations, dur_count, dur_avg = zip(
+            *[(d, c, a) for d, c, a in zip(durations, dur_count, dur_avg) if a != min(dur_avg)]
+        )
+
     for i in range(batch_size * length):
-        res[i, np.random.randint(DEFAULT_DURATION_CLUSTER_SIZE)] = 1
+        dur = np.random.choice(durations, p=dur_count)
+        res[i, dur] = 1
 
     return res.reshape((batch_size, length, DEFAULT_DURATION_CLUSTER_SIZE))
 
 
-def wrap_seed_generator_w_duration(seed_gen):
-    def inner(length, input_size, **kwargs):
-        batch_size = kwargs['batch_size'] or 16
-        seed = seed_gen(length, input_size, **kwargs)
-        dur_length = seed.shape[1]
-        duration = get_random_duration(dur_length, batch_size)
+def wrap_seed_generator_w_duration(duration_dict, ignore_shortest):
+    def inner(seed_gen):
+        def inner2(length, input_size, **kwargs):
+            batch_size = kwargs['batch_size'] or 16
+            seed = seed_gen(length, input_size, **kwargs)
+            dur_length = seed.shape[1]
+            duration = get_random_duration(
+                dur_length, batch_size, duration_dict, ignore_shortest)
 
-        return np.concatenate((seed, duration), axis=2)
+            return np.concatenate((seed, duration), axis=2)
+
+        return inner2
 
     return inner
 
@@ -93,29 +108,32 @@ def multi_note_simult_harmonic_seed(length, input_size, batch_size=16):
     return res
 
 
-seed_generators = {
-    "zero_seed": wrap_seed_generator_w_duration(zero_seed),
-    "random_noise_seed": wrap_seed_generator_w_duration(random_noise_seed),
-    "band_noise_seed": wrap_seed_generator_w_duration(band_noise_seed),
+def get_seed_generators(duration_dict, ignore_shortest=True):
+    wrapper = wrap_seed_generator_w_duration(duration_dict, ignore_shortest)
 
-    "single_note_seed": wrap_seed_generator_w_duration(single_note_seed),
-    "multi_note_seed": wrap_seed_generator_w_duration(multi_note_seed),
-    "multi_note_harmonic_seed": wrap_seed_generator_w_duration(multi_note_harmonic_seed),
-    "multi_note_simult_seed": wrap_seed_generator_w_duration(multi_note_simult_seed),
-    "multi_note_simult_harmonic_seed": wrap_seed_generator_w_duration(multi_note_simult_harmonic_seed),
+    return {
+        "zero_seed": wrapper(zero_seed),
+        "random_noise_seed": wrapper(random_noise_seed),
+        "band_noise_seed": wrapper(band_noise_seed),
 
-    "single_note_seed_noise": wrap_seed_generator_w_duration(random_noise_adder(single_note_seed)),
-    "multi_note_seed_noise": wrap_seed_generator_w_duration(random_noise_adder(multi_note_seed)),
-    "multi_note_harmonic_seed_noise": wrap_seed_generator_w_duration(random_noise_adder(multi_note_harmonic_seed)),
-    "multi_note_simult_seed_noise": wrap_seed_generator_w_duration(random_noise_adder(multi_note_simult_seed)),
-    "multi_note_simult_harmonic_seed_noise": wrap_seed_generator_w_duration(random_noise_adder(multi_note_simult_harmonic_seed)),
+        "single_note_seed": wrapper(single_note_seed),
+        "multi_note_seed": wrapper(multi_note_seed),
+        "multi_note_harmonic_seed": wrapper(multi_note_harmonic_seed),
+        "multi_note_simult_seed": wrapper(multi_note_simult_seed),
+        "multi_note_simult_harmonic_seed": wrapper(multi_note_simult_harmonic_seed),
 
-    "single_note_seed_band_noise": wrap_seed_generator_w_duration(band_noise_adder(single_note_seed)),
-    "multi_note_seed_band_noise": wrap_seed_generator_w_duration(band_noise_adder(multi_note_seed)),
-    "multi_note_harmonic_seed_band_noise": wrap_seed_generator_w_duration(band_noise_adder(multi_note_harmonic_seed)),
-    "multi_note_simult_seed_band_noise": wrap_seed_generator_w_duration(band_noise_adder(multi_note_simult_seed)),
-    "multi_note_simult_harmonic_seed_band_noise": wrap_seed_generator_w_duration(band_noise_adder(multi_note_simult_harmonic_seed)),
-}
+        "single_note_seed_noise": wrapper(random_noise_adder(single_note_seed)),
+        "multi_note_seed_noise": wrapper(random_noise_adder(multi_note_seed)),
+        "multi_note_harmonic_seed_noise": wrapper(random_noise_adder(multi_note_harmonic_seed)),
+        "multi_note_simult_seed_noise": wrapper(random_noise_adder(multi_note_simult_seed)),
+        "multi_note_simult_harmonic_seed_noise": wrapper(random_noise_adder(multi_note_simult_harmonic_seed)),
+
+        "single_note_seed_band_noise": wrapper(band_noise_adder(single_note_seed)),
+        "multi_note_seed_band_noise": wrapper(band_noise_adder(multi_note_seed)),
+        "multi_note_harmonic_seed_band_noise": wrapper(band_noise_adder(multi_note_harmonic_seed)),
+        "multi_note_simult_seed_band_noise": wrapper(band_noise_adder(multi_note_simult_seed)),
+        "multi_note_simult_harmonic_seed_band_noise": wrapper(band_noise_adder(multi_note_simult_harmonic_seed)),
+    }
 
 # _, axs = plt.subplots(nrows=4, ncols=4, figsize=(20, 20), subplot_kw={'xticks':[], 'yticks':[]})
 # for ax, (name, gen) in zip(axs.flat, sg.items()):
