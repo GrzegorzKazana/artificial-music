02394faaacf74356eecd10743b913440957f0c57
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2019-11-10 08:37:41 +0100

    added ability to retrive vector similarities during decoding

--- src/data_processing/sparse_notes_classified_time/embed_adapt.py
+++ src/data_processing/sparse_notes_classified_time/embed_adapt.py
@@ -2,20 +2,24 @@ import numpy as np
 from src.data_processing.embedding_sparse_notes.common import hash_frame, map_hashed_frame_to_names, unhash_named_frame, UNKNOWN_FRAME, TRACK_END
 
 
-def decode_note_vector_track(note_vecs, wv):
+def decode_note_vector_track(note_vecs, wv, return_similarities=False):
     res = []
+    sims = []
     for note_vec in note_vecs:
-        token = wv.similar_by_vector(note_vec, topn=1)[0][0]
+        token, similarity = wv.similar_by_vector(note_vec, topn=1)[0]
         if token == UNKNOWN_FRAME and len(res) == 0:
             res.append(unhash_named_frame(''))
+            sims.append(similarity)
         elif token == UNKNOWN_FRAME:
             res.append(res[-1])
+            sims.append(sims[-1])
         elif token == TRACK_END:
             break
         else:
             res.append(unhash_named_frame(token))
+            sims.append(similarity)
 
-    return np.array(res)
+    return np.array(res) if not return_similarities else (np.array(res), np.array(sims))
 
 
 def encode_note_frame(frame, wv):
